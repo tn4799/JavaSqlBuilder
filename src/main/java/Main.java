3bec1f81e3e209b0f55c280974d1a41e6acfdb21
Tim Nolle <tn4799@gmail.com>
2026-02-14 16:56:34 +0100

    changed Main-Class structure to match Java21 syntax

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,17 @@ import sqlbuilder.SelectBuilder;
 import sqlbuilder.dialects.SqlDialect;
 import sqlbuilder.expressions.Expression;
 
-void main() {
-    SelectBuilder builder = new SelectBuilder(new SqlDialect.OracleDialect());
+public class Main {
+    public static void main(String[] args) {
+        SelectBuilder builder = new SelectBuilder(new SqlDialect.OracleDialect());
 
-    Query query = builder
-            .select("A")
-            .from("TABLE")
-            .where(Expression.eq("C", 3).and().neq("C", 4))
-            .where(Expression.gt("E", "ABC"))
-            .build();
+        Query query = builder
+                .select("A")
+                .from("TABLE")
+                .where(Expression.eq("C", 3).and().neq("C", 4))
+                .where(Expression.gt("E", "ABC"))
+                .build();
 
-    IO.println(query.getStatement());
+        System.out.println(query.getStatement());
+    }
 }
