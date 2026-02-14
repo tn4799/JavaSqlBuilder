e8d989d6d7d00825cccfff61183551105050694c
Tim Nolle <tn4799@gmail.com>
2026-02-14 17:11:34 +0100

    added support for handling tableNames as alias in DB2
    For example, tableName.columnName will cause problems when using DB2 if
    the tableName was not defined as an clear alias.

--- src/main/java/sqlbuilder/dialects/SqlDialect.java
+++ src/main/java/sqlbuilder/dialects/SqlDialect.java
@@ -1,8 +1,15 @@
 package sqlbuilder.dialects;
 
+import sqlbuilder.exceptions.ValueCannotBeEmptyException;
+
+import java.util.Set;
+
 public interface SqlDialect {
     default public String quote(String identifier) {
         return "\"" + identifier + "\"";
+    }
+    default public String formatTableIdentifier(String tableIdentifier, Set<String> tableContext) {
+        return tableIdentifier;
     }
 
     /**
@@ -27,6 +34,23 @@ public interface SqlDialect {
         public String applyPaging(int limit, int offset) {
             //TODO implement paging syntax
             return "";
+        }
+
+        @Override
+        public String formatTableIdentifier(String tableIdentifier, Set<String> tableContext) {
+            if(tableIdentifier == null || tableIdentifier.isBlank()) {
+                throw new ValueCannotBeEmptyException("tableIdentifier");
+            }
+
+            for(String tableName : tableContext) {
+                String prefix = tableName + ".";
+
+                if(tableIdentifier.startsWith(prefix)) {
+                    return tableIdentifier.substring(prefix.length());
+                }
+            }
+
+            return tableIdentifier;
         }
     }
 
