a8a9ddb1a78891ae5748306751193ab994bba424
Tim Nolle <tn4799@gmail.com>
2026-02-14 20:12:03 +0100

    changed parameters from map to list
    This change had been made to not force the user to manually count the
    number of parameters currently used, especially when there are
    parameters that are added by the statement builder when constructing the
    statement with fixed values.
    Disadvantage: The order of the parameters matter.

--- src/main/java/sqlbuilder/Query.java
+++ src/main/java/sqlbuilder/Query.java
@@ -1,22 +1,23 @@
 package sqlbuilder;
 
 import java.sql.ResultSet;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Query {
     private final String STATEMENT;
-    private Map<Integer, Object> parameters;
+    private final List<Object> parameters = new ArrayList<>();
 
     public Query(String statement) {
         this.STATEMENT = statement;
     }
 
-    public void setParameter(int idx, Object parameter) {
-        if(!parameters.containsKey(idx)) {
-            throw new IllegalArgumentException("Unknown parameter index cannot be set.");
-        }
+    public void addParameter(Object parameter) {
+        parameters.add(parameter);
+    }
 
-        parameters.put(idx, parameter);
+    public List<Object> getParameters() {
+        return parameters;
     }
 
     public String getStatement() {
