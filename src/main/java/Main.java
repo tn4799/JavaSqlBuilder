26c7e1583c173e82e743fdebf267579e0ac32379
Tim Nolle <tn4799@gmail.com>
2026-02-15 17:28:12 +0100

    Extended example on usage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,17 +3,34 @@ import sqlbuilder.SelectBuilder;
 import sqlbuilder.dialects.SqlDialect;
 import sqlbuilder.expressions.Expression;
 
+import static sqlbuilder.expressions.Expression.*;
+
 public class Main {
     public static void main(String[] args) {
-        SelectBuilder builder = new SelectBuilder(new SqlDialect.OracleDialect());
+        SqlDialect.OracleDialect dialect = new SqlDialect.OracleDialect();
+        SelectBuilder builder = new SelectBuilder(dialect);
+        SelectBuilder subQuery = new SelectBuilder(dialect);
+
+        subQuery
+                .select("col1")
+                .from("table")
+                .where(eq("fuenf", 5)
+                        .or().eq("fuenf", 6))
+                ;
 
         Query query = builder
-                .select("A")
+                //.select("A")
                 .from("TABLE")
                 .where(Expression.eq("C", 3).and().neq("C", 4))
-                .where(Expression.gt("E", "ABC"))
+                .where(Expression.gt("E", "ABC").and().leq("F", Expression.param(4)))
+                .where(Expression.eq("CAST", Expression._case().whenThen(Expression.isNull("HAUS"), Expression.column("AUTO"))._else("HAUS")))
+                .where(Expression.in("I", 1,2,3).or().like("J", Expression.column("K")))
+                .where(Expression.in("BASE",
+                        subQuery
+                ))
                 .build();
 
-        System.out.println(query.getStatement());
+        System.out.println("prepared statement: " + query.getStatement());
+        System.out.println("populated statement: " + query.getPopulatedStatement());
     }
 }
