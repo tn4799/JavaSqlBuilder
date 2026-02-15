2275684c7254e5b7254ca3a70feabd1ef404927a
Tim Nolle <tn4799@gmail.com>
2026-02-15 17:20:46 +0100

    added method to get statement in populated state

--- src/main/java/sqlbuilder/Query.java
+++ src/main/java/sqlbuilder/Query.java
@@ -3,13 +3,16 @@ package sqlbuilder;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Query {
-    private final String STATEMENT;
+    private final String statement;
     private final List<Object> parameters = new ArrayList<>();
 
-    public Query(String statement) {
-        this.STATEMENT = statement;
+    public Query(String statement, List<Object> parameters) {
+        this.statement = statement;
+        this.parameters.addAll(parameters);
     }
 
     public void addParameter(Object parameter) {
@@ -21,7 +24,33 @@ public class Query {
     }
 
     public String getStatement() {
-        return STATEMENT;
+        return statement;
+    }
+
+    public String getPopulatedStatement() {
+        StringBuffer populatedStatement = new StringBuffer();
+        Matcher matcher = Pattern.compile("\\?").matcher(statement);
+
+        for(Object param : parameters) {
+            if(param == null) {
+                // move the matcher forward to ignore prepared statement parameter
+                matcher.find();
+                continue;
+            }
+            String parameter = param instanceof String ? "'%s'".formatted(param) : param.toString();
+            replacePreparedParameter(matcher, populatedStatement, parameter);
+        }
+
+        matcher.appendTail(populatedStatement);
+        return populatedStatement.toString();
+    }
+
+    private void replacePreparedParameter(Matcher matcher, StringBuffer buffer, String replacement) {
+        if(!matcher.find()) {
+            return;
+        }
+
+        matcher.appendReplacement(buffer, replacement);
     }
 
     public ResultSet execute() {
